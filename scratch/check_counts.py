40af70e7e3e8cdbd8bdb3b8bd36f5985e97430d1
litvin <sergltvn@gmail.com>
2026-04-22 12:27:58 +0300

    repo cleanup remove scratch db checks and gitignore kilo

--- scratch/check_counts.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('manga.db')
-c = conn.cursor()
-for table in ['chapters_urls', 'ranobe_urls']:
-    c.execute(f'SELECT count(*) FROM {table}')
-    print(f"{table}: {c.fetchone()[0]}")
-conn.close()
